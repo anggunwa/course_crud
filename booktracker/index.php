c2cca32191880818fd9df3d59bffd53292cea4eb
anggunwa <anggunwahyua@gmail.com>
2025-06-20 15:03:04 +0700

    tambah insert

--- booktracker/index.php
+++ booktracker/index.php
@@ -32,3 +32,51 @@ $stmt = $conn->query("SELECT * FROM books ORDER BY FIELD(status, 'reading', 'fin
 $books = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book List</title>
+</head>
+<body>
+
+
+    <h1>📖 Book List</h1>
+
+    <?php if(!empty($success)): ?>
+        <p style="color:green"><?= htmlspecialchars($success) ?></p>
+    <?php endif; ?>
+    <?php if(!empty($error)): ?>
+        <p style="color:red"><?= htmlspecialchars($error) ?></p>
+    <?php endif; ?>
+    
+    <form method="POST">
+        <h3>Enter the book information:</h3>
+        <h4>Book title?</h4>
+        <input name="title" placeholder="Title"><br>
+        <h4>Book status?</h4>
+        <input type="radio" id="reading" name="status" value="reading">
+        <label for="reading">Reading</label>
+        <input type="radio" id="finished" name="status" value="finished">
+        <label for="finished">Finished</label><br><br>
+        <button type="submit">Add book</button>
+    </form>
+    <br>
+    <hr>
+    <h2>List of books:</h2>
+    <ul>
+        <?php foreach($books as $book): ?>
+            <li>
+                <?php if ($book["status"] === "reading"): ?>
+                    📕 <?= htmlspecialchars($book['title']) ?> [<?= htmlspecialchars($book['status']) ?>]
+                <?php else: ?>
+                    📗 <?= htmlspecialchars($book['title']) ?> [<?= htmlspecialchars($book['status']) ?>]
+                <?php endif; ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+
+</body>
+</html>
